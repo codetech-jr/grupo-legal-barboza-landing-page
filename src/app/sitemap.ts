7c1efd9a0f82350d08502c89e77a00c7417ee2e6
codetech-jr <codetechjunior2000@gmail.com>
2026-04-10 20:31:42 -0400

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://grupolegalbarboza.com'
+  const baseUrl = 'https://www.grupolegalbarboza.com'
   
   return [
     {
